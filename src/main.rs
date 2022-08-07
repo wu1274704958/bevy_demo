22384d544ef9dc5adf05ee50aafe0d65e2bff0f9
wws <1274704958@qq.com>
2022-08-07 22:16:33 +0800

    up 0.8

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 use bevy::{prelude::*, render::pass::ClearColor};
 
 fn main() {
-    App::build()
+    App::new()
         .add_resource(ClearColor(Color::rgb(0.5, 0.5, 0.9)))
         .add_plugins(DefaultPlugins)
         .run();
